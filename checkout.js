ff231803e4769cae1325a5557e10f4254a6f2dac
kawan <kawanfreitas108@gmail.com>
2024-07-04 09:53:13 -0300

    asasd

--- checkout.js
+++ checkout.js
@@ -8,21 +8,22 @@ checkoutButtons.forEach(function(button) {
     customCheckoutButton.style.backgroundColor = '#007bff'; // Altera a cor de fundo do botão
     customCheckoutButton.style.color = '#ffffff'; // Altera a cor do texto do botão
     // Adicione mais estilos conforme desejado
-});
 
+    customCheckoutButton.addEventListener('click', function (event) {
+        event.preventDefault(); // Impede o comportamento padrão
 
-checkoutButtons.addEventListener('click', function (event) {
-    event.preventDefault(); // Impede o comportamento padrão
+        // Obter os dados do carrinho
+        fetch('/cart.js')
+            .then(response => response.json())
+            .then(cart => {
+                var customCheckoutUrl = 'https://dev-pay.spacefy.com.br/checkout?cart=' + encodeURIComponent(JSON.stringify(cart));
+                window.location.href = customCheckoutUrl;
+            })
+            .catch(error => {
+                console.error('Erro ao obter os dados do carrinho:', error);
+                window.location.href = '/checkout';
+            });
+    });
 
-    // Obter os dados do carrinho
-    fetch('/cart.js')
-        .then(response => response.json())
-        .then(cart => {
-            var customCheckoutUrl = 'https://dev-pay.spacefy.com.br/checkout?cart=' + encodeURIComponent(JSON.stringify(cart));
-            window.location.href = customCheckoutUrl;
-        })
-        .catch(error => {
-            console.error('Erro ao obter os dados do carrinho:', error);
-            window.location.href = '/checkout';
-        });
+    button.parentNode.replaceChild(customCheckoutButton, button);
 });
